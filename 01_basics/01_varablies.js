068cc920d40550ad871dbcb2204db39ad97acef5
chetan <jupiter6438@gmail.com>
2025-03-21 19:18:57 +0530

    init

---
+++ 01_basics/01_varablies.js
@@ -0,0 +1,7 @@
+let accountId = 142454 // in const it cannot change value
+let account = "vchetan989@gmail.com"
+var accountPassword = "12345"
+const accountCity = "hisar" 
+
+accountId = 2
+console.log(accountId);
