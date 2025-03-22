56b3b54c07f8c1b02c99a410c921b20247537bbb
chetan <jupiter6438@gmail.com>
2025-03-22 21:36:03 +0530

    new

--- 01_basics/01_varablies.js
+++ 01_basics/01_varablies.js
@@ -1,7 +1,13 @@
-let accountId = 142454 // in const it cannot change value
-let account = "vchetan989@gmail.com"
+// never use var 
+const accountId = 142454 // in const it cannot change value
+let accountEmail = "vchetan989@gmail.com"
 var accountPassword = "12345"
-const accountCity = "hisar" 
-
-accountId = 2
+accountCity = "hisar" 
+let accountState;
+//accountId = 2 // not allowed
+accountEmail = "sjlkj@gmail.com"
+accountPassword = "23231215"
+accountCity = "hansi" 
 console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
