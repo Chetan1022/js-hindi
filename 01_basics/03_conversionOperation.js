297e8902baffc7b69cf6e94b26c8c66e45b4e46e
chetan <jupiter6438@gmail.com>
2025-03-23 14:46:41 +0530

    new

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = true
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); 
+//console.log(valueInNumber); 
+
+//                  NOTES
+// "33" = 33
+// "33as" = NaN
+// true/false = 0/1
+
+let isLoggedIn = "chetan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
