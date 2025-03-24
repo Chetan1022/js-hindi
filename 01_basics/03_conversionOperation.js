8fe5ee27d58350805a2439e8b201444db9e6df77
chetan <jupiter6438@gmail.com>
2025-03-24 18:22:52 +0530

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,5 +13,44 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "chetan"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+
+
+
+// *****************************Operations**********************************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = " chetan"
+let str3 = str1+str2
+console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
