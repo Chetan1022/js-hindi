be95e50661f5a677cc468ecbd8df14cc5d41fcf2
chetan <jupiter6438@gmail.com>
2025-03-26 20:30:32 +0530

    comparsion

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,17 @@
+//                                 **COMPARSION**
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+
+//                                ** not use this type of comparsion **
+console.log("2" >1);
+console.log("02" >1);
+
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+console.log(undefined==0);
+
+console.log("2" === 2);
+
